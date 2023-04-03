f689809898770e587eea6aab671fac2cc62a16ed
kiloccnp <kiloccnp@gmail.com>
2023-04-03 13:39:41 +0700

    Update installer.tf

--- install/installer.tf
+++ install/installer.tf
@@ -3,6 +3,11 @@ resource "null_resource" "openshift_installer" {
     command = "wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.6.28/openshift-install-linux-4.6.28.tar.gz"
   }
 
+    provisioner "local-exec" {
+    command = "mv openshift-install-linux-4.6.28.tar.gz ./installer-files/"
+  }
+  
+  
   provisioner "local-exec" {
     command = "tar zxvf ${path.root}/installer-files//openshift-install-*-4*.tar.gz -C ${path.root}/installer-files/"
   }
@@ -16,7 +21,10 @@ resource "null_resource" "openshift_client" {
   provisioner "local-exec" {
     command = "wget https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.6.28/openshift-client-linux-4.6.28.tar.gz"
   }
-
+  provisioner "local-exec" {
+    command = "mv openshift-client-linux-4.6.28.tar.gz ./installer-files/"
+  }
+  
   provisioner "local-exec" {
     command = "tar zxvf ${path.root}/installer-files//openshift-client-*-4*.tar.gz -C ${path.root}/installer-files/"
   }
